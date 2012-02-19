80255fd98cfc6f7aa4501dd85725a819e0ec1d6c
Carmen Alvarez <calvarez@Carmen-Alvarezs-Mac-mini.local>
2012-02-19 18:26:08 +0100

    changed prefix from N to NN.

--- src/main.c
+++ src/main.c
@@ -5,9 +5,9 @@
  *      Author: calvarez
  */
 #include <stdio.h>
-#include "nimage.h"
+#include "nnimage.h"
 int main(int argc, char **argv) {
-	NImage *image = nimage_new("someid", "somefile");
+	NNImage *image = nnimage_new("someid", "somefile");
 	printf("Hello %s\n", image->filename);
 	return 0;
 }
