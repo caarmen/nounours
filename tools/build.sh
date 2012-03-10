74ba319ac49cb3eaa6e940db8585cabacae57a59
Carmen Alvarez <c@rmen.ca>
2012-03-10 17:31:34 +0100

    automate integrating with xscreensaver

--- tools/build.sh
+++ tools/build.sh
@@ -1,6 +1,6 @@
 aclocal -I /opt/local/share/aclocal &&
 automake -a &&
 autoconf &&
-./configure --with-jpeg-include-path=/opt/local/include --with-jpeg-lib-path=/opt/local/lib &&
+./configure --with-jpeg-include-path=/opt/local/include --with-jpeg-lib-path=/opt/local/lib --with-xscreensaver-config-path=/opt/local/share/xscreensaver/config &&
 make &&
 ./nounours
