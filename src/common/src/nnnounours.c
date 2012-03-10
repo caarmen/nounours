3e559a8250d08301b805d22b7afe89732d6a38dc
Carmen Alvarez <c@rmen.ca>
2012-03-10 14:53:56 +0100

    trying some corrections for sleeping

--- src/common/src/nnnounours.c
+++ src/common/src/nnnounours.c
@@ -26,7 +26,8 @@ static void nnnounours_reset_idle(NNNounours *nounours) {
 	nounours->last_action_time_us = now_tv.tv_sec * 1000000 + now_tv.tv_usec;
 }
 
-NNNounours * nnnounours_new(const char *path, int screensaver_mode, int window_id) {
+NNNounours * nnnounours_new(const char *path, int screensaver_mode,
+		int window_id) {
 	NNNounours *nounours = malloc(sizeof(NNNounours));
 	nounours->screensaver_mode = screensaver_mode;
 	nounours->do_stretch = 0;
@@ -56,7 +57,7 @@ NNNounours * nnnounours_new(const char *path, int screensaver_mode, int window_i
 
 void nnnounours_use_theme(NNNounours *nounours, NNTheme *theme) {
 	nounours->cur_theme = theme;
-	if(nounours->do_stretch)
+	if (nounours->do_stretch)
 		nnuinounours_stretch(nounours->uinounours);
 	else
 		nnuinounours_resize(nounours->uinounours, theme->width, theme->height);
@@ -82,6 +83,9 @@ static void *nnnounours_animation_thread(void *data) {
 	nnanimation_start(animation);
 	nounours->is_doing_animation = 0;
 	nounours->cur_animation = 0;
+	if (animation == nounours->cur_theme->animation_idle)
+		nnnounours_reset_idle(nounours);
+
 	return (void*) NULL;
 }
 void nnnounours_start_animation(NNNounours *nounours, NNAnimation *animation) {
@@ -131,7 +135,7 @@ void nnnounours_on_move(NNNounours *nounours, int x, int y) {
 	long now_us = now_tv.tv_sec * 1000000 + now_tv.tv_usec;
 	if (nounours->last_x >= 0) {
 		long time_diff = now_us - nounours->last_motion_event_time_us;
-		if(time_diff == 0)
+		if (time_diff == 0)
 			time_diff = 1;
 		float vel_x = 1000000 * (x - nounours->last_x) / time_diff;
 		float vel_y = 1000000 * (y - nounours->last_y) / time_diff;
@@ -171,12 +175,16 @@ void nnnounours_on_shake(NNNounours *nounours) {
 }
 
 void nnnounours_ping(NNNounours *nounours) {
-	if (nounours->cur_theme == 0 || nounours->is_doing_animation || nounours->cur_image == 0)
+	if (nounours->cur_theme == 0 || nounours->is_doing_animation
+			|| nounours->cur_image == 0)
 		return;
 	struct timeval now_tv;
 	gettimeofday(&now_tv, NULL);
 	long now_us = now_tv.tv_sec * 1000000 + now_tv.tv_usec;
 	long time_diff = now_us - nounours->last_action_time_us;
+	printf("last action time %ld vs sleep time %d vs ping time %d, diff time %ld\n",
+			nounours->last_action_time_us, nounours->idle_time*1000,
+			nounours->idle_ping_interval*1000, time_diff);
 	if (time_diff > nounours->idle_time * 1000) {
 		nnnounours_stop_animation(nounours);
 		nnnounours_start_animation(nounours,
