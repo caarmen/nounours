30461e42288be1ca40f5c95e400b27d9a7b4c231
ubuntu <ubuntu@ubuntu-VirtualBox.(none)>
2012-04-28 22:30:13 +0200

    working on debian packaging

--- tools/build.sh
+++ tools/build.sh
@@ -1,6 +1,27 @@
-aclocal -I /opt/local/share/aclocal &&
+configure_flags= 
+aclocal_flags=
+if [ `uname` = "Darwin" ]
+then
+  configure_flags="--with-jpeg-include-path=/opt/local/include --with-jpeg-lib-path=/opt/local/lib --with-xscreensaver-config-path=/opt/local/share/xscreensaver/config"
+  aclocal_flags="-I /opt/local/share/aclocal"
+fi
+
+aclocal $aclocal_flags &&
 automake -a &&
 autoconf &&
-./configure --with-jpeg-include-path=/opt/local/include --with-jpeg-lib-path=/opt/local/lib --with-xscreensaver-config-path=/opt/local/share/xscreensaver/config &&
-make &&
-./nounours
+./configure $configure_flags &&
+make 
+if [ $? -eq 0 ]
+then
+  if [ $# = 1 ]
+  then 
+    if [ $1 = "dist" ]
+    then
+     cd debian
+     debuild
+    fi
+  else 
+    ./nounours
+  fi
+fi
+
