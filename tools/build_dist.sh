bd5e9a5af35ac4147ffffcb928f0015561ac0e2c
ubuntu <ubuntu@ubuntu-VirtualBox.(none)>
2012-04-28 23:50:57 +0200

    build repo

--- tools/build_dist.sh
+++ tools/build_dist.sh
@@ -7,7 +7,7 @@ cd $top
 version=`cat VERSION` 
 dist_dir=dist/nounours-$version
 mkdir -p $dist_dir
-find . -type f |grep -v "dist\|.git\|debian\|tools" |xargs cp --parents -t $dist_dir
+find . -type f |grep -v "dist\|.git\|debian\|tools\|repo" |xargs cp --parents -t $dist_dir
 cd $dist_dir
 env LOGNAME="$me" DEBFULLNAME="$me" dh_make -e c@rmen.ca -s --createorig
 cd $top
@@ -15,4 +15,9 @@ mkdir -p $dist_dir/debian
 cp -pr debian $dist_dir/.
 cd $dist_dir
 debuild
+dpkg-scanpackages . > Packages
 cd $top
+mkdir -p repo/dists/stable/main/binary-i386
+cp dist/*.deb repo/.
+cp $dist_dir/Packages debian/Release repo/dists/stable/main/binary-i386/.
+tar czvf repo.tar.gz repo
