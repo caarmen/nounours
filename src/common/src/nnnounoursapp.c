234c1aae0e227017bb367312d9ea5392c292900a
Carmen Alvarez <c@rmen.ca>
2012-05-09 01:18:46 +0200

    icons per theme

--- src/common/src/nnnounoursapp.c
+++ src/common/src/nnnounoursapp.c
@@ -60,8 +60,9 @@ void nnnounoursapp_use_theme_scaled(NNNounoursApp *app, NNTheme *theme, float sc
 	if(!app->config.is_in_screensaver_mode) {
 		char icon_filename[1024];
 		sprintf(icon_filename,
-				"%s/nounours/data/icons/nounours.xpm",
-				__DATAROOT_DIR__);
+				"%s/nounours/%s/images/nounours.xpm",
+				__DATAROOT_DIR__,
+				theme->path);
 		nnwindow_set_icon(app->ui, icon_filename);
 	}
 	int i, j;
