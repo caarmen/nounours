d60162d0bc69af9291a3b562840d58cf1db8e9c1
Carmen Alvarez <c@rmen.ca>
2012-05-08 21:28:27 +0200

    md5sum

--- tools/build_repo.sh
+++ tools/build_repo.sh
@@ -5,6 +5,7 @@ then
 fi
 VERSION=1.0-1_i386
 REPO_PATH=$1
+REPO_PATH=`echo $REPO_PATH | sed -e 's|/|\\\/|g'`
 echo "repo: ${REPO_PATH}"
 here=`dirname $0`
 cd $here
@@ -18,12 +19,13 @@ cp debian/Release $top/dist/nounours_$VERSION.deb $repo_dir/
 cp debian/changelog $repo_dir/nounours_$VERSION.changelog
 cd $repo_dir
 dpkg-scanpackages . > Packages
-cat Packages |sed -e 's/Filename: /Filename: '${REPO_PATH}'/g' > Packages.tmp
+cat Packages |sed -e 's/Filename: \./Filename: '${REPO_PATH}'/g' > Packages.tmp
 mv Packages.tmp Packages
 gzip -c Packages > Packages.gz
 rm -f Release.gpg
-echo "MD5Sum:" >> Release
-md5sum * >> Release
+cat Release | sed -e 's/Codename: .*$/Codename: '${REPO_PATH}'/g' > Release.tmp
+mv Release.tmp Release
+apt-ftparchive release . >> Release
 gpg -abs -o Release.gpg Release
 tar czvf repo.tar.gz *
 cd $top
