7153da42cf07edd1950f11667a4416fa0b53cb94
Carmen Alvarez <c@rmen.ca>
2012-05-10 23:25:51 +0200

    nounours xscreensaver config

--- tools/clean.sh
+++ tools/clean.sh
@@ -1,10 +1,11 @@
 make clean
-rm Makefile
-rm data/icons/Makefile
+rm -f Makefile
+rm -f data/Makefile
+rm -f data/icons/Makefile
 rm -rf autom4te.cache
-rm config.log
-rm config.status
-rm config/config.h
-rm config/stamp-h1
+rm -f config.log
+rm -f config.status
+rm -f config/config.h
+rm -f config/stamp-h1
 rm -rf .deps  **/.deps **/**/.deps **/**/**/.deps dist repo
-rm **/.dirstamp **/**/**/.dirstamp
+rm -f **/.dirstamp **/**/**/.dirstamp
